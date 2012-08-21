76c0aeff42cda0dd675e72be3e265c5f4062c1e1
elishowk <elishowk@nonutc.fr>
2012-08-21 13:12:47 +0200

    videotag UI templatetags

--- templatetags/videotag.py
+++ templatetags/videotag.py
@@ -3,10 +3,10 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('videotag/videotag.html')
-def videotagwidget(id, classes):
-    return { 'id': id, 'classes': classes }
+@register.inclusion_tag('videotag/ticker.html')
+def videotagticker(id, classes, tickerid, tickerclasses):
+    return { 'id': id, 'classes': classes, 'tickerid': tickerid, 'tickerclasses': tickerclasses }
 
 @register.inclusion_tag('videotag/postform.html')
-def videotagpostform(id):
-    return { 'id': id }
+def videotagpostform(id, classes):
+    return { 'id': id, 'classes': classes }
