6a5dad45707b17fdfee77d85f502fc9e3976eb31
elishowk <elishowk@nonutc.fr>
2012-08-21 18:27:47 +0200

    merged activitybar with videotag, ticker and postform

--- templatetags/videotag.py
+++ templatetags/videotag.py
@@ -4,8 +4,8 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('videotag/ticker.html')
-def videotagticker(id, classes, tickerid, tickerclasses):
-    return { 'id': id, 'classes': classes, 'tickerid': tickerid, 'tickerclasses': tickerclasses }
+def videotagticker(id, classes):
+    return { 'id': id, 'classes': classes }
 
 @register.inclusion_tag('videotag/postform.html')
 def videotagpostform(id, classes):
